1d4687e76353b5cf857a20adbc9ee48a94125658
heini-phys-chem <heini.phys.chem@gmail.com>
2017-04-12 12:55:55 +0200

    add colors

--- mcFuck.py
+++ mcFuck.py
@@ -1,4 +1,15 @@
 import sys
+import random
+
+class bcolors:
+    HEADER = '\033[95m'
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    RED = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+
+colors = [bcolors.HEADER, bcolors.BLUE, bcolors.GREEN, bcolors.RED, bcolors.FAIL, bcolors.ENDC]
 
 NumOfFucksGiven = raw_input("How many Fucks do you want to give today? ")
 NumOfFucksParalell = raw_input("And how many of them in paralell? ")
@@ -7,7 +18,7 @@ max = int(float(NumOfFucksGiven) / float(NumOfFucksParalell))
 rest = int(NumOfFucksGiven) % int(NumOfFucksParalell)
 
 for i in range(0, int(max)):
-    print "Fuck! " * int(NumOfFucksParalell) + str(i)
+    print random.choice(colors) + "Fuck! " * int(NumOfFucksParalell) + str(i) + bcolors.ENDC
 
 if (rest == 0):
     print "Congratulations, Number of Fucks given today = " + NumOfFucksGiven 
