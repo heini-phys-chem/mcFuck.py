d7b83126938ba0d318076851e9c6ac213c0efc17
heini-phys-chem <heini.phys.chem@gmail.com>
2021-01-21 13:04:20 +0100

    Jaaaaaaaan

--- mcFuck.py
+++ mcFuck.py
@@ -11,19 +11,23 @@ class bcolors:
 
 colors = [bcolors.HEADER, bcolors.BLUE, bcolors.GREEN, bcolors.RED, bcolors.FAIL, bcolors.ENDC]
 
-NumOfFucksGiven = raw_input("How many Fucks do you want to give today? ")
-NumOfFucksParalell = raw_input("And how many of them in paralell? ")
+NumOfFucksGiven    = input("How many Fucks do you want to give today? ")
+NumOfFucksParalell = input("And how many of them in paralell? ")
+
+if int(NumOfFucksGiven) == 0:
+    print('You are a lame Wanker!')
+    exit()
 
 maximumFucks = int(float(NumOfFucksGiven) / float(NumOfFucksParalell))
 rest = int(NumOfFucksGiven) % int(NumOfFucksParalell)
 
 for i in range(0, int(maximumFucks)):
-    print random.choice(colors) + "Fuck! " * int(NumOfFucksParalell) + str(i) + bcolors.ENDC
+    print(random.choice(colors) + "Fuck! " * int(NumOfFucksParalell) + str(i) + bcolors.ENDC)
 
 if (rest == 0):
-    print "Congratulations, Number of Fucks given today = " + NumOfFucksGiven 
+    print("Congratulations, Number of Fucks given today = " + NumOfFucksGiven)
 else:
-    print "Fuck! " * rest
-    print "Congratulations, Number of Fucks given today = " + NumOfFucksGiven 
+    print("Fuck! " * rest)
+    print("Congratulations, Number of Fucks given today = " + NumOfFucksGiven)
 
 
